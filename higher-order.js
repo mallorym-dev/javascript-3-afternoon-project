d006e58ad900d90000a831730f1836b6f9a8ccb7
unknown <malloryjmathews@gmail.com>
2019-08-16 18:36:10 -0600

    javascript 3 part 3

--- higher-order.js
+++ higher-order.js
@@ -20,7 +20,9 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-//Code Here
+const mixedNumbers = mixedNumbers.filter (function(el, i, arr)) {
+  if (el === )
+}
 let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
 
 
